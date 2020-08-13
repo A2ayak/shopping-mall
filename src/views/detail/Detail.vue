19140fc6948cb637989a4aee8084155d8c434361
A2ayak <429853312@qq.com>
2020-08-13 18:41:16 +0800

    third

---
+++ src/views/detail/Detail.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{iid}}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Detail",
+    data() {
+      return {
+        iid: null
+      }
+    },
+    create() {
+      this.iid = this.$router.params.iid
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
